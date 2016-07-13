8b2c2686511f3a4cd831ed6437f1e7c631cfb47b
Gandhi Anderson <thegands@gmail.com>
2016-07-13 01:20:17 -0600

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(name) {
+  return "I love " + name;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(decimal) {
+  return Math.floor(decimal);
+}
+
+function theTruth(decimal) {
+  return true;
+}
